f710a0200a040bba060acd0c09cdd6ab54ff5619
louisgaignard1-glitch <louisgaignard1@gmail.com>
2026-02-23 15:35:39 +0100

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 def display_metrics(result_min_var, mu, Sigma, returns):
     if result_min_var.success:
-        from utils.optimization import calculate_sharpe_ratio, calculate_max_drawdown
+        from optimization import calculate_sharpe_ratio, calculate_max_drawdown
 
         weights = result_min_var.x
         portfolio_returns = returns.dot(weights)
